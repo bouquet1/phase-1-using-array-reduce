0b695acc0e1570f8a537137fbdc51a6bbb8a00de
bouquet1 <bukuleta@gmail.com>
2023-04-14 21:37:34 -0700

    completed .reduce()

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// const batteries = () => batteryBatches.reduce(function(accumulator, element){
+//     const totalBatteries = accumulator + element;
+//     return totalBatteries
+// })
+// console.log(batteries);
+
+// function batteries(arr){
+//     const totalBatteries = arr.reduce(function(accumulator, element){
+//         return element + accumulator;
+//     });
+//     return totalBatteries;
+// }
+
+// console.log(batteries(batteryBatches));
+
+
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator}, 0)
+
+console.log(totalBatteries);
+
